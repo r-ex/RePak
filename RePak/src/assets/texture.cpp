282e4273de2c793354469d318afb24b1ec1d5cb5
Rika <34874943+IJARika@users.noreply.github.com>
2022-08-20 14:32:15 -0400

    Mip map support (#6)
    * way better cpu data struct, by like a lot.
    
    * fully dynamic materials plus cpu data updates.
    
    * minor typo
    
    * type condensing and cleanup
    
    also adds some error conditions so people don't make broken materials.
    
    * not a valid in
    
    * functional mipmaps
    
    this is probably jank af but it works so shrug
    
    * Add files via upload
    
    * sreamed textures!!! also apex formant copy over.
    
    * better streaming handling and cleanup of unused test vars.
    
    * not where it should have been
    
    * added (untested) apex support,  more cleanup
    
    * added (untested) apex support, more cleanup
    
    * argh
    
    * fix logging
    
    * again
    
    * add disabling streaming
    
    might be useful in certain cases shrug
    
    * fix epic classes
    
    * update apex data struct
    
    * classes + apex cpudata struct
    
    * formatting
    
    * notes, better surface2 handling, and formatting.
    
    * typo hehe
    
    * slight cleanup
    
    * starpak path
    
    * notes
    
    * small cleanup
    
    * cleanup txtr format selection
    
    * fix typo
    
    * Update texture.cpp
    
    * wip improved format detection
    
    * support more dxgi formats
    
    * Revert "Merge branch 'local-materials-and-mips' into mip-map-support"
    
    This reverts commit bbd09b6550b6b31e0c0e763864194d5a10d96216, reversing
    changes made to be2f955aca4b2b34cc63a68a1beb64e13b2af262.
    
    Co-authored-by: rexx <67599507+r-ex@users.noreply.github.com>

--- RePak/src/assets/texture.cpp
+++ RePak/src/assets/texture.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 #include "rmem.h"
 #include "Assets.h"
+#include "dxutils.h"
 
-// VERSION 7
 void Assets::AddTextureAsset_v8(std::vector<RPakAssetEntry>* assetEntries, const char* assetPath, rapidjson::Value& mapEntry)
 {
     Log("Adding txtr asset '%s'\n", assetPath);
@@ -25,6 +25,12 @@ void Assets::AddTextureAsset_v8(std::vector<RPakAssetEntry>* assetEntries, const
 
     std::string sAssetName = assetPath; // todo: this needs to be changed to the actual name
 
+    uint32_t nLargestMipSize = 0;
+    uint32_t nStreamedMipSize = 0;
+    uint32_t nDDSHeaderSize = 0;
+
+    bool bStreamable = false;
+
     // parse input image file
     {
         int magic;
@@ -35,95 +41,136 @@ void Assets::AddTextureAsset_v8(std::vector<RPakAssetEntry>* assetEntries, const
 
         DDS_HEADER ddsh = input.read<DDS_HEADER>();
 
-        hdr->m_nDataLength = ddsh.pitchOrLinearSize;
-        hdr->m_nWidth = ddsh.width;
-        hdr->m_nHeight = ddsh.height;
-        Log("-> dimensions: %ix%i\n", ddsh.width, ddsh.height);
+        int nStreamedMipCount = 0;
+
+        if (ddsh.dwMipMapCount > 9)
+        {
+            if (mapEntry.HasMember("disableStreaming") && mapEntry["disableStreaming"].GetBool())
+            {
+                nStreamedMipCount = 0;
+                bStreamable = false;
+            }
+            else
+            {
+                nStreamedMipCount = ddsh.dwMipMapCount - 9;
+                bStreamable = true;
+            }
+        }
+
+        uint32_t nTotalSize = 0;
+        for (int ml = 0; ml < ddsh.dwMipMapCount; ml++)
+        {
+            uint32_t nCurrentMipSize = (ddsh.dwPitchOrLinearSize / std::pow(4, ml));
+
+            // add 16 bytes if mip data size is below 8 bytes else add calculated size
+            nTotalSize += (nCurrentMipSize <= 8 ? 16 : nCurrentMipSize);
+
+            // if this texture and mip are streaming
+            if (bStreamable && ml < (ddsh.dwMipMapCount - 9))
+                nStreamedMipSize += nCurrentMipSize;
+        }
+
+        hdr->m_nDataLength = nTotalSize;
+        hdr->m_nWidth = ddsh.dwWidth;
+        hdr->m_nHeight = ddsh.dwHeight;
+
+        Log("-> dimensions: %ix%i\n", ddsh.dwWidth, ddsh.dwHeight);
+
+        hdr->m_nPermanentMipLevels = (ddsh.dwMipMapCount - nStreamedMipCount);
+        hdr->m_nStreamedMipLevels = nStreamedMipCount;
+
+        Log("-> total mipmaps permanent:streamed : %i:%i\n", hdr->m_nPermanentMipLevels, hdr->m_nStreamedMipLevels);
+
+        nLargestMipSize = ddsh.dwPitchOrLinearSize;
 
         DXGI_FORMAT dxgiFormat;
 
-        // Checks if the texture is DX10+, this is needed for SRGB.
-        if (ddsh.pixelfmt.fourCC == '01XD') {
-
-            DDS_HEADER_DXT10 ddsh_dx10 = input.read<DDS_HEADER_DXT10>();
-
-            switch (ddsh_dx10.dxgiFormat)
-            {
-            case 72:
-                Log("-> fmt: BC1 SRGB\n");
-                dxgiFormat = DXGI_FORMAT_BC1_UNORM_SRGB;
-                break;
-            case 75:
-                Log("-> fmt: BC2 SRGB\n");
-                dxgiFormat = DXGI_FORMAT_BC2_UNORM_SRGB;
-                break;
-            case 78:
-                Log("-> fmt: BC2 SRGB\n");
-                dxgiFormat = DXGI_FORMAT_BC3_UNORM_SRGB;
-                break;
-            case 98:
-                Log("-> fmt: BC7\n");
-                dxgiFormat = DXGI_FORMAT_BC7_UNORM;
-                break;
-            case 99:
-                Log("-> fmt: BC7 SRGB\n");
-                dxgiFormat = DXGI_FORMAT_BC7_UNORM_SRGB;
-                break;
-            default:
+        switch (ddsh.ddspf.dwFourCC)
+        {
+        case '1TXD': // DXT1
+            dxgiFormat = DXGI_FORMAT_BC1_UNORM;
+            break;
+        case '3TXD': // DXT3
+            dxgiFormat = DXGI_FORMAT_BC2_UNORM;
+            break;
+        case '5TXD': // DXT5
+            dxgiFormat = DXGI_FORMAT_BC3_UNORM;
+            break;
+        case 'U4CB': // BC4U
+            dxgiFormat = DXGI_FORMAT_BC4_UNORM;
+            break;
+        case 'S4CB':
+            dxgiFormat = DXGI_FORMAT_BC4_SNORM;
+            break;
+        case '2ITA': // ATI2
+        case 'U5CB': // BC5U
+            dxgiFormat = DXGI_FORMAT_BC5_UNORM;
+            break;
+        case 'S5CB': // BC5S
+            dxgiFormat = DXGI_FORMAT_BC5_SNORM;
+            break;
+        case '01XD': // DX10
+            dxgiFormat = DXGI_FORMAT_UNKNOWN;
+            break;
+        // legacy format codes
+        case 36:
+            dxgiFormat = DXGI_FORMAT_R16G16B16A16_UNORM;
+            break;
+        case 110:
+            dxgiFormat = DXGI_FORMAT_R16G16B16A16_SNORM;
+            break;
+        case 111:
+            dxgiFormat = DXGI_FORMAT_R16_FLOAT;
+            break;
+        case 112:
+            dxgiFormat = DXGI_FORMAT_R16G16_FLOAT;
+            break;
+        case 113:
+            dxgiFormat = DXGI_FORMAT_R16G16B16A16_FLOAT;
+            break;
+        case 114:
+            dxgiFormat = DXGI_FORMAT_R32_FLOAT;
+            break;
+        case 115:
+            dxgiFormat = DXGI_FORMAT_R32G32_FLOAT;
+            break;
+        case 116:
+            dxgiFormat = DXGI_FORMAT_R32G32B32A32_FLOAT;
+            break;
+        default:
+            dxgiFormat = dxutils::GetFormatFromHeader(ddsh);
+            
+            if(dxgiFormat == DXGI_FORMAT_UNKNOWN)
                 Error("Attempted to add txtr asset '%s' that was not using a supported DDS type. Exiting...\n", assetPath);
-                break;
-            }
-
+            
+            return;
         }
-        // Non SRGB texture processing.
-        else {
-
-            switch (ddsh.pixelfmt.fourCC)
-            {
-            case '1TXD':
-                Log("-> fmt: DXT1\n");
-                dxgiFormat = DXGI_FORMAT_BC1_UNORM;
-                break;
-            case '3TXD':
-                Log("-> fmt: DXT3\n");
-                dxgiFormat = DXGI_FORMAT_BC2_UNORM;
-                break;
-            case '5TXD':
-                Log("-> fmt: DXT5\n");
-                dxgiFormat = DXGI_FORMAT_BC3_UNORM;
-                break;
-            case 'U4CB':
-                Log("-> fmt: BC4U\n");
-                dxgiFormat = DXGI_FORMAT_BC4_UNORM;
-                break;
-            case 'U5CB':
-                Log("-> fmt: BC5U\n");
-                dxgiFormat = DXGI_FORMAT_BC5_UNORM;
-                break;
-            case 'S5CB':
-                Log("-> fmt: BC5U\n");
-                dxgiFormat = DXGI_FORMAT_BC5_SNORM;
-                break;
-            default:
-                Error("Attempted to add txtr asset '%s' that was not using a supported DDS type. Exiting...\n", assetPath);
-                return;
-            }
-        }
-
-        hdr->m_nFormat = s_txtrFormatMap[dxgiFormat];
 
         // Go to the end of the main header.
-        input.seek(ddsh.size + 4);
+        input.seek(ddsh.dwSize + 4);
+
+        // this is used for some math later
+        nDDSHeaderSize = ddsh.dwSize + 4;
 
         // Go to the end of the DX10 header if it exists.
-        if (ddsh.pixelfmt.fourCC == '01XD')
-            input.seek(20, std::ios::cur);
+        if (ddsh.ddspf.dwFourCC == '01XD')
+        {
+            DDS_HEADER_DXT10 ddsh_dx10 = input.read<DDS_HEADER_DXT10>();
+
+            dxgiFormat = ddsh_dx10.dxgiFormat;
+
+            if (s_txtrFormatMap.count(dxgiFormat) == 0)
+                Error("Attempted to add txtr asset '%s' using unsupported DDS type '%s'. Exiting...\n", assetPath, dxutils::GetFormatAsString(dxgiFormat).c_str());
+
+            nDDSHeaderSize += 20;
+        }
+
+        Log("-> fmt: %s\n", dxutils::GetFormatAsString(dxgiFormat).c_str());
+
+        hdr->m_nFormat = s_txtrFormatMap[dxgiFormat];
     }
 
     hdr->m_nAssetGUID = RTech::StringToGuid((sAssetName + ".rpak").c_str());
-
-    // unfortunately i'm not a respawn engineer so 1 (unstreamed) mip level will have to do
-    hdr->m_nPermanentMipLevels = 1;
 
     bool bSaveDebugName = mapEntry.HasMember("saveDebugName") && mapEntry["saveDebugName"].GetBool();
 
@@ -146,11 +193,50 @@ void Assets::AddTextureAsset_v8(std::vector<RPakAssetEntry>* assetEntries, const
 
     // woo more segments
     // cpu data
-    _vseginfo_t dataseginfo = RePak::CreateNewSegment(hdr->m_nDataLength, 3, 16);
 
-    char* databuf = new char[hdr->m_nDataLength];
+    _vseginfo_t dataseginfo = RePak::CreateNewSegment(hdr->m_nDataLength - nStreamedMipSize, 3, 16);
 
-    input.getReader()->read(databuf, hdr->m_nDataLength);
+    char* databuf = new char[hdr->m_nDataLength - nStreamedMipSize];
+
+    char* streamedbuf = new char[nStreamedMipSize];
+
+    int currentDDSOffset = 0;
+    int remainingDDSData = hdr->m_nDataLength;
+    int remainingStreamedData = nStreamedMipSize;
+
+    for (int ml = 0; ml < (hdr->m_nPermanentMipLevels + hdr->m_nStreamedMipLevels); ml++)
+    {
+        uint32_t nCurrentMipSize = (nLargestMipSize / std::pow(4, ml));
+        uint32_t mipSizeDDS = 0;
+        uint32_t mipSizeRpak = 0;
+
+        if (nCurrentMipSize <= 8)
+        {
+            currentDDSOffset += 8;
+            mipSizeDDS = 8;
+            mipSizeRpak = 16;
+        }
+        else
+        {
+            currentDDSOffset += nCurrentMipSize;
+            mipSizeDDS = nCurrentMipSize;
+            mipSizeRpak = nCurrentMipSize;
+        }
+
+        remainingDDSData -= mipSizeRpak;
+
+        input.seek(nDDSHeaderSize + (currentDDSOffset - mipSizeDDS), std::ios::beg);
+
+        if (bStreamable && ml < hdr->m_nStreamedMipLevels)
+        {
+            remainingStreamedData -= nCurrentMipSize;
+            input.getReader()->read(streamedbuf + remainingStreamedData, mipSizeDDS);
+        }
+        else
+        {
+            input.getReader()->read(databuf + remainingDDSData, mipSizeDDS);
+        }
+    }
 
     RePak::AddRawDataBlock({ subhdrinfo.index, subhdrinfo.size, (uint8_t*)hdr });
 
@@ -167,7 +253,22 @@ void Assets::AddTextureAsset_v8(std::vector<RPakAssetEntry>* assetEntries, const
     // now time to add the higher level asset entry
     RPakAssetEntry asset;
 
+    // this should hopefully fix some crashing
     uint64_t starpakOffset = -1;
+
+    if (bStreamable)
+    {
+        std::string sStarpakPath = "paks/Win64/repak.starpak";
+
+        // check per texture just in case for whatever reason you want stuff in different starpaks (if it ever gets fixed).
+        if (mapEntry.HasMember("starpakpath"))
+            sStarpakPath = mapEntry["starpakpath"].GetString();
+
+        RePak::AddStarpakReference(sStarpakPath);
+
+        SRPkDataEntry de{ -1, nStreamedMipSize, (uint8_t*)streamedbuf };
+        starpakOffset = RePak::AddStarpakDataEntry(de);
+    }
 
     asset.InitAsset(RTech::StringToGuid((sAssetName + ".rpak").c_str()), subhdrinfo.index, 0, subhdrinfo.size, dataseginfo.index, 0, starpakOffset, -1, (std::uint32_t)AssetType::TEXTURE);
     asset.m_nVersion = TXTR_VERSION;
@@ -178,4 +279,5 @@ void Assets::AddTextureAsset_v8(std::vector<RPakAssetEntry>* assetEntries, const
     assetEntries->push_back(asset);
 
     input.close();
+    printf("\n");
 }
