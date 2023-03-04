cc34b3866d8914fbb8d3bcb9085d080e31eebb95
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-03-04 10:17:15 +0100

    Cleanup 'ASSET_HANDLER' macro
    * Removed extraneous copy constructor for string, could directly check upon char pointer.
    * Use 'm_Header.fileVersion' instead of 'm_Version'. 'm_Version' is redundant and will be removed from the class soon.

--- src/application/repak.h
+++ src/application/repak.h
@@ -6,12 +6,11 @@
 // starpak data entry vector
 inline std::vector<SRPkDataEntry> g_vSRPkDataEntries{};
 
-// !TODO: remove extraneous copy constructor for 'ext'?
 #define ASSET_HANDLER(ext, file, assetEntries, func_v7, func_v8) \
-	if (file["$type"].GetStdString() == std::string(ext)) \
+	if (file["$type"].GetStdString() == ext) \
 	{ \
-		if(this->m_Version == 8 && func_v8) \
+		if(this->m_Header.fileVersion == 8 && func_v8) \
 			func_v8(this, &assetEntries, file["path"].GetString(), file); \
-		if(this->m_Version == 7 && func_v7) \
+		if(this->m_Header.fileVersion == 7 && func_v7) \
 			func_v7(this, &assetEntries, file["path"].GetString(), file); \
 	}
