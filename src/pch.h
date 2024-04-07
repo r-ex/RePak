0a137985b5d3240d060b83774d07b5bfa8e7cbb7
r-ex <67599507+r-ex@users.noreply.github.com>
2024-04-07 23:21:09 +0100

    remove windows.h include

--- src/pch.h
+++ src/pch.h
@@ -1,13 +1,12 @@
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+//#include <windows.h>
 #include <d3d11.h>
 
 #include <filesystem>
 #include <iostream>
 #include <unordered_map>
-#include <sysinfoapi.h>
+//#include <sysinfoapi.h>
 #include <vector>
 #include <cstdint>
 #include <string>
