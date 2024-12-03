e7e5f2f8ee6606bb70d3368f290b1839898ec520
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-12-03 12:34:04 +0100

    Model asset code refactor
    - Use BinaryIO class for reading RMDL and Vertex Groups
    - Allow sequences, rigs and materials to be parsed from strings and guids
    - Mark "animrigs", "materials" and "sequences" as overridable by prepending '$' to them.
    - Add warning when number of anim rigs do not match the number defined in studiohdr.

--- src/assets/model.cpp
+++ src/assets/model.cpp
@@ -5,81 +5,97 @@
 
 char* Model_ReadRMDLFile(const std::string& path)
 {
-    REQUIRE_FILE(path);
+    BinaryIO modelFile;
 
-    size_t fileSize = Utils::GetFileSize(path);
+    if (!modelFile.Open(path, BinaryIO::Mode_e::Read))
+        Error("Failed to open model file \"%s\"\n", path.c_str());
+
+    const size_t fileSize = modelFile.GetSize();
 
     if (fileSize < sizeof(studiohdr_t))
-        Error("invalid model file '%s'. must be at least %i bytes, found %zu\n", path.c_str(), sizeof(studiohdr_t), fileSize);
+        Error("Invalid model file \"%s\"; must be at least %zu bytes, found %zu\n", path.c_str(), sizeof(studiohdr_t), fileSize);
 
-    char* buf = new char[fileSize];
+    char* const buf = new char[fileSize];
+    modelFile.Read(buf, fileSize);
 
-    std::ifstream ifs(path, std::ios::in | std::ios::binary);
-    ifs.read(buf, fileSize);
-    ifs.close();
-
-    studiohdr_t* pHdr = reinterpret_cast<studiohdr_t*>(buf);
+    studiohdr_t* const pHdr = reinterpret_cast<studiohdr_t*>(buf);
 
     if (pHdr->id != 'TSDI') // "IDST"
-        Error("invalid model file '%s'. expected magic %x, found %x\n", path.c_str(), 'TSDI', pHdr->id);
+        Error("Invalid model file \"%s\"; expected magic %x, found %x\n", path.c_str(), 'TSDI', pHdr->id);
 
     if (pHdr->version != 54)
-        Error("invalid model file '%s'. expected version %i, found %i\n", path.c_str(), 54, pHdr->version);
+        Error("Invalid model file \"%s\"; expected version %i, found %i\n", path.c_str(), 54, pHdr->version);
 
     if (pHdr->length > fileSize)
-        Error("invalid model file '%s'. studiohdr->length(%i) > fileSize(%i)\n", path.c_str(), pHdr->length, fileSize);
+        Error("Invalid model file \"%s\"; studiohdr->length(%i) > fileSize(%i)\n", path.c_str(), pHdr->length, fileSize);
 
     return buf;
 }
 
-char* Model_ReadVGFile(const std::string& path, size_t* pFileSize)
+char* Model_ReadVGFile(const std::string& path, size_t* const pFileSize)
 {
-    REQUIRE_FILE(path);
+    BinaryIO vgFile;
 
-    size_t fileSize = Utils::GetFileSize(path);
+    if (!vgFile.Open(path, BinaryIO::Mode_e::Read))
+        Error("Failed to open vertex group file \"%s\"\n", path.c_str());
+
+    const size_t fileSize = vgFile.GetSize();
 
     if (fileSize < sizeof(VertexGroupHeader_t))
-        Error("invalid model file '%s'. must be at least %zu bytes, found %zu\n", path.c_str(), sizeof(VertexGroupHeader_t), fileSize);
+        Error("Invalid vertex group file \"%s\"; must be at least %zu bytes, found %zu\n", path.c_str(), sizeof(VertexGroupHeader_t), fileSize);
 
-    char* buf = new char[fileSize];
+    char* const buf = new char[fileSize];
+    vgFile.Read(buf, fileSize);
 
-    std::ifstream ifs(path, std::ios::in | std::ios::binary);
-    ifs.read(buf, fileSize);
-    ifs.close();
-
-    VertexGroupHeader_t* pHdr = reinterpret_cast<VertexGroupHeader_t*>(buf);
+    VertexGroupHeader_t* const pHdr = reinterpret_cast<VertexGroupHeader_t*>(buf);
 
     if (pHdr->id != 'GVt0') // "0tVG"
-        Error("invalid model file '%s'. expected magic %x, found %x\n", path.c_str(), 'GVt0', pHdr->id);
+        Error("Invalid vertex group file \"%s\"; expected magic %x, found %x\n", path.c_str(), 'GVt0', pHdr->id);
 
     // not sure if this is actually version but i've also never seen it != 1
     if (pHdr->version != 1)
-        Error("invalid model file '%s'. expected version %i, found %i\n", path.c_str(), 1, pHdr->version);
+        Error("Invalid vertex group file \"%s\"; expected version %i, found %i\n", path.c_str(), 1, pHdr->version);
 
     *pFileSize = fileSize;
     return buf;
 }
 
+static void Model_CheckAssetRef(const rapidjson::Value& val, const char* assetType, const int index)
+{
+    if (!val.IsNumber())
+    {
+        if (!val.IsString())
+            Error("%s #%i is of unsupported type; expected %s or %s, found %s\n", assetType, index,
+                JSON_TypeToString(JSONFieldType_e::kUint64), JSON_TypeToString(JSONFieldType_e::kString),
+                JSON_TypeToString(JSON_ExtractType(val)));
+
+        if (val.GetStringLength() == 0)
+            Error("%s #%i was defined as an invalid empty string\n", assetType, index);
+    }
+}
+
 bool Model_AddSequenceRefs(CPakDataChunk* chunk, CPakFile* pak, ModelAssetHeader_t* hdr, const rapidjson::Value& mapEntry)
 {
-    if (!mapEntry.HasMember("sequences") || !mapEntry["sequences"].IsArray())
+    rapidjson::Value::ConstMemberIterator sequencesIt;
+    const bool hasSequences = JSON_GetIterator(mapEntry, "$sequences", JSONFieldType_e::kArray, sequencesIt);
+
+    if (!hasSequences)
         return false;
 
+    const rapidjson::Value::ConstArray sequencesArray = sequencesIt->value.GetArray();
     std::vector<PakGuid_t> sequenceGuids;
 
-    for (auto& it : mapEntry["sequences"].GetArray())
+    int seqIndex = -1;
+    for (const auto& sequence : sequencesArray)
     {
-        if (!it.IsString())
-            continue;
-
-        if (it.GetStringLength() == 0)
-            continue;
+        seqIndex++;
+        Model_CheckAssetRef(sequence, "sequence", seqIndex);
 
         PakGuid_t guid;
 
-        if (!JSON_ParseNumber(it, guid))
+        if (!JSON_ParseNumber(sequence, guid))
         {
-            const char* const sequencePath = it.GetString();
+            const char* const sequencePath = sequence.GetString();
             Log("Auto-adding aseq asset \"%s\".\n", sequencePath);
 
             guid = RTech::StringToGuid(sequencePath);
@@ -93,7 +109,7 @@ bool Model_AddSequenceRefs(CPakDataChunk* chunk, CPakFile* pak, ModelAssetHeader
     CPakDataChunk guidsChunk = pak->CreateDataChunk(sizeof(PakGuid_t) * sequenceGuids.size(), SF_CPU, 64);
 
     PakGuid_t* pGuids = reinterpret_cast<PakGuid_t*>(guidsChunk.Data());
-    for (int i = 0; i < sequenceGuids.size(); ++i)
+    for (size_t i = 0; i < sequenceGuids.size(); ++i)
     {
         pGuids[i] = sequenceGuids[i];
     }
@@ -107,23 +123,21 @@ void Assets::AddModelAsset_v9(CPakFile* const pak, const char* const assetPath, 
     CPakDataChunk hdrChunk = pak->CreateDataChunk(sizeof(ModelAssetHeader_t), SF_HEAD, 16);
     ModelAssetHeader_t* pHdr = reinterpret_cast<ModelAssetHeader_t*>(hdrChunk.Data());
 
+    std::vector<PakGuidRefHdr_t> guids;
+
     std::string rmdlFilePath = pak->GetAssetPath() + assetPath;
-
-    // VG is a "fake" file extension that's used to store model streaming data (name came from the magic '0tVG')
-    // this data is a combined mutated version of the data from .vtx and .vvd in regular source models
-    std::string vgFilePath = Utils::ChangeExtension(rmdlFilePath, "vg");
-
-    REQUIRE_FILE(vgFilePath);
-
-    std::vector<PakGuidRefHdr_t> guids{};
-
     char* rmdlBuf = Model_ReadRMDLFile(rmdlFilePath);
+
     studiohdr_t* studiohdr = reinterpret_cast<studiohdr_t*>(rmdlBuf);
 
     ///--------------------
     // Add VG data
+    // VG is a "fake" file extension that's used to store model streaming data (name came from the magic '0tVG')
+    // this data is a combined mutated version of the data from .vtx and .vvd in regular source models
+    std::string vgFilePath = Utils::ChangeExtension(rmdlFilePath, "vg");
+
     size_t vgFileSize = 0;
-    char* vgBuf = Model_ReadVGFile(vgFilePath, &vgFileSize);
+    char* const vgBuf = Model_ReadVGFile(vgFilePath, &vgFileSize);
 
     //
     // Physics
@@ -132,7 +146,7 @@ void Assets::AddModelAsset_v9(CPakFile* const pak, const char* const assetPath, 
 
     CPakDataChunk phyChunk;
     
-    if (JSON_GetValueOrDefault(mapEntry, "usePhysics", false))
+    if (studiohdr->vphyindex)
     {
         BinaryIO phyInput;
         const std::string physicsFile = Utils::ChangeExtension(rmdlFilePath, "phy");
@@ -152,36 +166,39 @@ void Assets::AddModelAsset_v9(CPakFile* const pak, const char* const assetPath, 
     CPakDataChunk animRigsChunk;
 
     rapidjson::Value::ConstMemberIterator it;
-    const bool hasAnimRigs = JSON_GetIterator(mapEntry, "animrigs", JSONFieldType_e::kArray, it);
+    const bool hasAnimRigs = JSON_GetIterator(mapEntry, "$animrigs", JSONFieldType_e::kArray, it);
 
     if (hasAnimRigs)
     {
         const rapidjson::Value::ConstArray animrigs = it->value.GetArray();
-        pHdr->animRigCount = (uint32_t)animrigs.Size();
+        const uint32_t numAnimrigs = (uint32_t)animrigs.Size();
 
-        animRigsChunk = pak->CreateDataChunk(animrigs.Size() * sizeof(PakGuid_t), SF_CPU, 64);
+        if (numAnimrigs != pHdr->animRigCount)
+            Warning("model expects %u animrigs, but only %u were provided; model may not work correctly\n", pHdr->animRigCount, numAnimrigs);
+
+        pHdr->animRigCount = numAnimrigs;
+
+        animRigsChunk = pak->CreateDataChunk(numAnimrigs * sizeof(PakGuid_t), SF_CPU, 64);
 
         rmem arigBuf(animRigsChunk.Data());
 
-        int i = 0;
+        int i = -1;
         for (const auto& animrig : animrigs)
         {
-            if (!animrig.IsString())
-                Error("invalid animrig entry for model '%s'\n", assetPath);
+            i++;
+            Model_CheckAssetRef(animrig, "animrig", i);
 
-            if (animrig.GetStringLength() == 0)
-                Error("anim rig #%i for model '%s' was defined as an invalid empty string\n", i, assetPath);
+            PakGuid_t guid;
 
-            const PakGuid_t guid = RTech::StringToGuid(animrig.GetStdString().c_str());
+            if (!JSON_ParseNumber(animrig, guid))
+                guid = RTech::StringToGuid(animrig.GetString());
 
             arigBuf.write<PakGuid_t>(guid);
 
             // check if anim rig is a local asset so that the relation can be added
-            PakAsset_t* animRigAsset = pak->GetAssetByGuid(guid);
+            PakAsset_t* const animRigAsset = pak->GetAssetByGuid(guid);
 
             pak->SetCurrentAssetAsDependentForAsset(animRigAsset);
-
-            i++;
         }
 
         pHdr->pAnimRigs = animRigsChunk.GetPointer();
@@ -261,27 +278,36 @@ void Assets::AddModelAsset_v9(CPakFile* const pak, const char* const assetPath, 
         pak->AddPointer(hdrChunk.GetPointer(offsetof(ModelAssetHeader_t, pPhyData)));
     }
 
-    bool hasMaterialOverrides = mapEntry.HasMember("materials");
+    rapidjson::Value::ConstMemberIterator materialsIt;
+
+    // todo(amos): de we even want material overrides? shouldn't these need to
+    // be fixed in the studiomdl itself? there are reports of this causing many
+    // errors as the game tries to read the path from the mdl itself which this
+    // loop below doesn't update.
+    const bool hasMaterialOverrides = JSON_GetIterator(mapEntry, "$materials", JSONFieldType_e::kArray, materialsIt);
+    const rapidjson::Value* materialOverrides = hasMaterialOverrides ? &materialsIt->value : nullptr;
 
     // handle material overrides register all material guids
     for (int i = 0; i < studiohdr->numtextures; ++i)
     {
         mstudiotexture_t* tex = studiohdr->pTexture(i);
 
-        // if material overrides are possible and this material has an entry in the array
-        if (hasMaterialOverrides && mapEntry["materials"].GetArray().Size() > static_cast<size_t>(i))
+        if (hasMaterialOverrides)
         {
-            auto& matlEntry = mapEntry["materials"].GetArray()[i];
+            rapidjson::Value::ConstArray materialArray = materialOverrides->GetArray();
 
-            // if string, calculate the guid
-            if (matlEntry.IsString())
+            if (materialArray.Size() > i)
             {
-                if (matlEntry.GetStringLength() != 0) // if no material path, use the original model material
-                    tex->guid = RTech::StringToGuid(std::string("material/" + matlEntry.GetStdString() + ".rpak").c_str()); // use user provided path
+                const rapidjson::Value& matlEntry = materialArray[i];
+                Model_CheckAssetRef(matlEntry, "material", i);
+
+                PakGuid_t guid;
+
+                if (!JSON_ParseNumber(matlEntry, guid))
+                    guid = RTech::StringToGuid(matlEntry.GetString());
+
+                tex->guid = guid;
             }
-            // if uint64, treat the value as the guid
-            else if (matlEntry.IsUint64())
-                tex->guid = matlEntry.GetUint64();
         }
 
         if (tex->guid != 0)
