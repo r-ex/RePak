36c2d95b880f8bb553102d3867dbcba5820af71c
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-01-26 00:03:26 +0100

    Add NextPowerOfTwo to common math
    Will be used to calculate the bucket size for settings layout field name hashing.

--- src/math/common.h
+++ src/math/common.h
@@ -6,3 +6,15 @@ inline bool IsPowerOfTwo(T x)
 	return (x & (x - 1)) == 0;
 }
 
+template <typename T>
+inline T NextPowerOfTwo(T x)
+{
+	x--;
+	x |= x >> 1;
+	x |= x >> 2;
+	x |= x >> 4;
+	x |= x >> 8;
+	x |= x >> 16;
+	x++;
+	return x;
+}
