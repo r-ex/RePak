0dda5a89f5741ba49c009ab221353ed7bcfdca69
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-01-31 16:56:46 +0100

    Fix bug in BinaryIO::Pad()
    We must always use the remainder when writing out padding. The bug was that it would still do the full count or PAD_BUF_SIZE at all times even though the remainder is 20 for example. This fixes the last few rare alignment problems.

--- src/utils/binaryio.cpp
+++ src/utils/binaryio.cpp
@@ -294,7 +294,7 @@ void BinaryIO::Pad(const size_t count)
 
 	while (remainder)
 	{
-		const size_t writeCount = (std::min)(count, PAD_BUF_SIZE);
+		const size_t writeCount = (std::min)(remainder, PAD_BUF_SIZE);
 		Write(s_padBuf, writeCount);
 
 		remainder -= writeCount;
