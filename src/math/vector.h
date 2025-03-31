fab8d24e06512394521e96fd6ae30e8d90b7115e
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-03-31 17:40:17 +0200

    Add Vector2 and QAngle class

--- src/math/vector.h
+++ src/math/vector.h
@@ -21,3 +21,46 @@ struct Vector3
 
 	float x, y, z;
 };
+
+struct Vector2
+{
+	Vector2()
+	{
+		this->Set(0.0f, 0.0f);
+	}
+
+	Vector2(float x, float y)
+	{
+		this->Set(x, y);
+	}
+
+	void Set(float newX, float newY)
+	{
+		this->x = newX;
+		this->y = newY;
+	}
+
+	float x, y;
+};
+
+struct QAngle
+{
+	QAngle()
+	{
+		this->Set(0.0f, 0.0f, 0.0f);
+	}
+
+	QAngle(float x, float y, float z)
+	{
+		this->Set(x, y, z);
+	}
+
+	void Set(float newX, float newY, float newZ)
+	{
+		this->x = newX;
+		this->y = newY;
+		this->z = newZ;
+	}
+
+	float x, y, z;
+};
