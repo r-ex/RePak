8f623c477dc326fea530c4702acd189f7ddbe273
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2025-07-10 22:55:10 +0200

    Match LCD field names with shader structure ( CBufEnginePost )
    Match the RLCD asset type field names with the member field names found in the CBufEnginePost shader class (this is indirectly mapped to the shader). This allows for clearer documentation and avoids confusion when looking up the CBufEnginePost member variables and comparing them with the ones found in LcdScreenEffect_s.

--- src/public/lcd_screen_effect.h
+++ src/public/lcd_screen_effect.h
@@ -2,16 +2,16 @@
 
 struct LcdScreenEffect_s
 {
-	float interlaceX;
-	float interlaceY;
-	float attenuation;
+	float pixelScaleX1;
+	float pixelScaleX2;
+	float pixelScaleY;
+	float brightness;
 	float contrast;
-	float gamma;
-	float washout;
-	float shutterBandingIntensity;
-	float shutterBandingFrequency;
-	float shutterBandingSpacing;
-	float exposure;
-	int reserved; // [amos]: always 0 and appears to do nothing in the runtime.
-	float noiseAmount;
+	float waveScale;
+	float waveOffset;
+	float waveSpeed;
+	float wavePeriod;
+	float bloomAdd;
+	uint32_t reserved; // [amos]: always 0 and appears to do nothing in the runtime.
+	float pixelFlicker;
 };
